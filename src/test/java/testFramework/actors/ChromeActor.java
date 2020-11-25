c501a14bb03fd8d1d66bd5156d3262afc3b469ef
Rosegoldthorp <code@koalatea-software.com>
2020-11-25 17:57:33 +0000

    fault "DevToolsActivePort file doesn't exist" - this may fix that

--- src/test/java/testFramework/actors/ChromeActor.java
+++ src/test/java/testFramework/actors/ChromeActor.java
@@ -89,10 +89,11 @@ public class ChromeActor extends Actor {
         // no sandbox tends to cure the random 'can't find open windows' failure
         options.addArguments("--no-sandbox"); //https://stackoverflow.com/a/50725918/1689770
         //                options.addArguments("--disable-infobars"); //https://stackoverflow.com/a/43840128/1689770
-        //                options.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
+        options.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
         //                options.addArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
         //                options.addArguments("--disable-gpu"); //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
         //                options.addArguments("enable-features=NetworkServiceInProcess");
+        //                options.addArguments("--disable-extensions");
 
         driver = new ChromeDriver(options);
 
